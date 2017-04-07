c5e0e76fbce314e9d54ac5eee9a6a57b54642794
Walee Ahmed <walee.tausif@gmail.com>
2017-04-06 23:09:06 -0400

    improved project structure w/ editor config

--- pages/events.vue
+++ pages/events.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
-    #events-page
-        h1 Events
+  #events-page
+    h1 Events
 </template>
 
 <script>
-export default {
-  components: {
-  },
-  data: () => ({
-  })
-}
+  export default {
+    components: {
+    },
+    data: () => ({
+    })
+  }
 </script>
 
 <style lang="stylus" scoped>
